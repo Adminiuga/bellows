e7a25cf140b231070d5dac8a9b0737b675927f91
Alexei Chetroi <alexei.chetroi@outlook.com>
2020-12-17 18:28:32 -0500

    Fix rebase

--- tests/test_application.py
+++ tests/test_application.py
@@ -98,9 +98,7 @@ async def _test_startup(app, nwk_type, ieee, auto_form=False, init=0, ezsp_versi
     ezsp_mock.addEndpoint = AsyncMock(return_value=t.EmberStatus.SUCCESS)
     ezsp_mock.setConfigurationValue = AsyncMock(return_value=t.EmberStatus.SUCCESS)
     ezsp_mock.networkInit = AsyncMock(return_value=[init])
-    ezsp_mock.getNetworkParameters = AsyncMock(
-        return_value=[0, nwk_type, sentinel.nework_parameters]
-    )
+    ezsp_mock.getNetworkParameters = mockezsp
     ezsp_mock.get_board_info = AsyncMock(
         return_value=("Mock Manufacturer", "Mock board", "Mock version")
     )
